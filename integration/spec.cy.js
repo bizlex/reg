4beb277bf683138775ec9c74b3d892e574ba5a43
Alexey Levchuk <bizlex@yandex.ru>
2023-09-15 23:44:17 +0200

    feat: remove cypress.env.json

--- integration/spec.cy.js
+++ integration/spec.cy.js
@@ -1,7 +1,17 @@
-describe('template spec', () => {
-  it('passes', () => {
+describe('spec', () => {
+  it('registrationMain', () => {
+    // region visit
+
     cy.visit('https://rejestracjapoznan.poznan.uw.gov.pl/')
+
+    // endregion
+
+    // region chooseOperation
+
     cy.contains('button', 'CUDZOZIEMCY - Złożenie wniosku:').click()
+
     cy.contains('button', 'Dalej').click()
+
+    // endregion
   })
 })
